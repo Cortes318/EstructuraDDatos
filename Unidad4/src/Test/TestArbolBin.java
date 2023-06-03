860f3daaf9ca276e915404ea71151abfe8699bdf
Ramon <Ramon@PC>
2023-06-02 20:33:44 -0600

    unidad4

--- Unidad4/src/Test/TestArbolBin.java
+++ Unidad4/src/Test/TestArbolBin.java
@@ -1,55 +1,82 @@
 package Test;
+
 import estrucnoLineal.ArbolBin;
 
 import javax.swing.*;
 
 import EntradaSalida.ToolsList;
 
-public class TestArbolBin {    
-	public static void main(String[] args) {
-    String menu = "Insertar,Recorridos,Buscar,Hojas,Altura,Ver,Salir";
-    menu3(menu);
-}
+public class TestArbolBin {
+    public static void main(String[] args) {
+        String menu = "Insertar,Recorridos,Buscar,Hojas,Altura,Nodo terminales,Nodos interiores,Ver,Salir";
+        menu3(menu);
+    }
+
     public static String boton(String menu) {
-        String valores[]=menu.split(",");
+        String valores[] = menu.split(",");
         int n;
-        n = JOptionPane.showOptionDialog(null," SELECCIONA DANDO CLICK ", " M E N U",
+        n = JOptionPane.showOptionDialog(null, "SELECCIONA DANDO CLICK", "M E N U",
                 JOptionPane.NO_OPTION,
-                JOptionPane.QUESTION_MESSAGE,null,
-                valores,valores[0]);
-        return ( valores[n]);
+                JOptionPane.QUESTION_MESSAGE, null,
+                valores, valores[0]);
+        return valores[n];
     }
 
-    public static void menu3(String menu)
-    {
-        ArbolBin<Integer> Nodito = new ArbolBin<Integer>();
+    public static void menu3(String menu) {
+        ArbolBin<Integer> arbol = new ArbolBin<Integer>();
 
-        String sel="";
+        String sel = "";
         do {
-            sel=boton(menu);
-            switch(sel){
+            sel = boton(menu);
+            switch (sel) {
                 case "Insertar": {
-                	System.out.println("El tipo de T es"+Nodito.getClass().getName());
-                	Nodito.insertarArbol(ToolsList.leerInt("Dato"));
-                	
+                    System.out.println("El tipo de T es " + arbol.getClass().getName());
+                    arbol.insertarArbol(ToolsList.leerInt("Dato"));
+                    break;
                 }
-                  
                 case "Recorridos": {
-                	ToolsList.imprimeMsg("\nInorden1:"+Nodito.preorden(Nodito.getRaiz()));
+                    ToolsList.imprimeMsg("\nInorden1: " + arbol.preorden(arbol.getRaiz()));
+                    break;
                 }
-                    
-                case "Buscar":
-                   
-                case "Hojas":
-                  
-                case "Altura":
-                 
-                case "Ver":
-                	
-                case "Salir":
+                case "Buscar": {
+                    Integer dato = ToolsList.leerInt("Ingrese el elemento a buscar: ");
+                    ArbolBin.Nodito nodoEncontrado = arbol.buscar(dato);
+                    if (nodoEncontrado != null) {
+                        System.out.println("Elemento encontrado: " + nodoEncontrado.getInfo());
+                    } else {
+                        System.out.println("Elemento no encontrado.");
+                    }
+                    break;
+                }
+                case "Hojas": {
+                    System.out.println("Hojas del árbol:");
+                    arbol.terminales(arbol.getRaiz());
+                    break;
+                }
+                case "Altura": {
+                    int altura = arbol.altura(arbol.getRaiz());
+                    System.out.println("Altura del árbol: " + altura);
+                    break;
+                }
+                case "Nodo terminales": {
+                    System.out.println("Modo terminales:");
+                    arbol.terminales(arbol.getRaiz());
+                    break;
+                }
+                case "Nodos interiores": {
+                    System.out.println("Nodos interiores:");
+                    arbol.interiores(arbol.getRaiz());
+                    break;
+                }
+                case "Ver": {
+                    arbol.visualizar();
+                    break;
+                }
+                case "Salir": {
+                    ToolsList.imprimeMsg("Saliendo del programa");
+                    break;
+                }
             }
-        }while(!sel.equalsIgnoreCase("Salir"));
+        } while (!sel.equals("Salir"));
     }
-
-
 }
--- Unidad4/src/estrucnoLineal/ArbolBin.java
+++ Unidad4/src/estrucnoLineal/ArbolBin.java
@@ -1,74 +1,169 @@
 package estrucnoLineal;
 
 public class ArbolBin<T> {
-	private Nodito raiz;
-	 
-	public ArbolBin() {
-		raiz=null;
-	}
-    
-	public Nodito getRaiz() {
-		return raiz;
-	}
-	 
-	public void setRaiz(Nodito raiz) {
-	 this.raiz=raiz;	
-	}
-	public boolean arbolVacio() {
-		return raiz==null;
-	}
-	public void vaciarArbol() {
-		raiz=null;
-	}
-	
-	public void insertarArbol(T info) {
-		Nodito p=new Nodito(info);
-		if(arbolVacio()) {
-			raiz=p;
-		}
-		else {
-			Nodito padre=buscaPadre(raiz,p);
-			if(p.hashCode()>=padre.hashCode())
-				padre.der=p;
-			else
-				padre.izq=p;
-		}
-	}
-	
-	public Nodito buscaPadre(Nodito actual,Nodito p) {
-		Nodito padre=null;
-		while(actual!=null) {
-			padre=actual;
-			if((int)p.info>=(int)padre.info)
-				actual=padre.der;
-			else
-				actual=padre.izq;
-		}
-		return padre;
-	}
-	public String preorden(Nodito r) {
-		if(r!=null) {
-			return r.info+"."+preorden(r.izq)+"."+preorden(r.der);
-		}
-		else return"";
-	}
-	public String inorden(Nodito r) {
-		if(r!=null) {
-			return inorden(r.izq)+" - "+r.getInfo()+" - "+inorden(r.der);
-			
-		}
-		else return"";
-	}
-	public String inorden2(Nodito r) {
-		if(r!=null) {
-			return inorden2(r.der)+" - "+r.getInfo()+" - "+inorden2(r.izq);
-		}
-		else return"";
-	}
-	public String postorden(Nodito r) {
-		if(r!=null) {
-			return r.getInfo()+" - "+postorden(r.izq)+" - "+postorden(r.der);
-		} else return"";
-	}
-}
+    private Nodito raiz;
 
+    public ArbolBin() {
+        raiz = null;
+    }
+
+    public Nodito getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(Nodito raiz) {
+        this.raiz = raiz;
+    }
+
+    public boolean arbolVacio() {
+        return raiz == null;
+    }
+
+    public void vaciarArbol() {
+        raiz = null;
+    }
+
+    public void insertarArbol(T info) {
+        Nodito p = new Nodito(info);
+        if (arbolVacio()) {
+            raiz = p;
+        } else {
+            Nodito padre = buscaPadre(raiz, p);
+            if (p.hashCode() >= padre.hashCode())
+                padre.der = p;
+            else
+                padre.izq = p;
+        }
+    }
+
+    public Nodito buscaPadre(Nodito actual, Nodito p) {
+        Nodito padre = null;
+        while (actual != null) {
+            padre = actual;
+            if ((int) p.info >= (int) padre.info)
+                actual = padre.der;
+            else
+                actual = padre.izq;
+        }
+        return padre;
+    }
+
+    public String preorden(Nodito r) {
+        if (r != null) {
+            return r.info + "." + preorden(r.izq) + "." + preorden(r.der);
+        } else return "";
+    }
+
+    public String inorden(Nodito r) {
+        if (r != null) {
+            return inorden(r.izq) + " - " + r.getInfo() + " - " + inorden(r.der);
+
+        } else return "";
+    }
+
+    public String inorden2(Nodito r) {
+        if (r != null) {
+            return inorden2(r.der) + " - " + r.getInfo() + " - " + inorden2(r.izq);
+        } else return "";
+    }
+
+    public String postorden(Nodito r) {
+        if (r != null) {
+            return r.getInfo() + " - " + postorden(r.izq) + " - " + postorden(r.der);
+        } else return "";
+    }
+    
+    public int altura(Nodito nodo) {
+        if (nodo == null)
+            return -1;
+        else {
+            int alturaIzq = altura(nodo.izq);
+            int alturaDer = altura(nodo.der);
+            return Math.max(alturaIzq, alturaDer) + 1;
+        }
+    }
+    
+    public void terminales(Nodito nodo) {
+        if (nodo != null) {
+            if (nodo.izq == null && nodo.der == null)
+                System.out.println(nodo.info);
+            terminales(nodo.izq);
+            terminales(nodo.der);
+        }
+    }
+    
+    public void interiores(Nodito nodo) {
+        if (nodo != null) {
+            if (nodo.izq != null || nodo.der != null)
+                System.out.println(nodo.info);
+            interiores(nodo.izq);
+            interiores(nodo.der);
+        }
+    }
+
+    public Nodito buscar(T dato) {
+        return buscarNodo(raiz, dato);
+    }
+
+    private Nodito buscarNodo(Nodito nodo, T dato) {
+        if (nodo == null || dato.equals(nodo.info)) {
+            return nodo;
+        }
+        Nodito nodoEncontrado = buscarNodo(nodo.izq, dato);
+        if (nodoEncontrado == null) {
+            nodoEncontrado = buscarNodo(nodo.der, dato);
+        }
+        return nodoEncontrado;
+    }
+
+    public void visualizar() {
+        visualizarArbol(raiz, 0);
+    }
+
+    private void visualizarArbol(Nodito nodo, int nivel) {
+        if (nodo != null) {
+            visualizarArbol(nodo.der, nivel + 1);
+            for (int i = 0; i < nivel; i++) {
+                System.out.print("    ");
+            }
+            System.out.println(nodo.info);
+            visualizarArbol(nodo.izq, nivel + 1);
+        }
+    }
+
+    public class Nodito {
+        private T info;
+        private Nodito izq;
+        private Nodito der;
+
+        public Nodito(T info) {
+            this.info = info;
+            izq = null;
+            der = null;
+        }
+
+        public T getInfo() {
+            return info;
+        }
+
+        public void setInfo(T info) {
+            this.info = info;
+        }
+
+        public Nodito getIzq() {
+            return izq;
+        }
+
+        public void setIzq(Nodito izq) {
+            this.izq = izq;
+        }
+
+        public Nodito getDer() {
+            return der;
+        }
+
+        public void setDer(Nodito der) {
+            this.der = der;
+        }
+    }
+}
